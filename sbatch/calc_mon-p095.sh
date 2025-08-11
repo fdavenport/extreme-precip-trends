8e78bf10830bbc408b00b738705cdd401fc56082
Frances Davenport <f.davenport@colostate.edu>
2025-08-11 11:22:44 -0600

    update sbatch to calculate every year

--- sbatch/calc_mon-p095.sh
+++ sbatch/calc_mon-p095.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=/davenport-scratch/fvdav22/job_output/calc_mon-p095.out
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=f.davenport@colostate.edu
-#SBATCH --ntasks=16
+#SBATCH --ntasks=32
 #SBATCH --time=40:00:00
-#SBATCH -p dav+coe
+#SBATCH -p dav_all,coe_all
 
 
 cd /davenport-scratch/fvdav22/projects/extreme-precip-trends
@@ -14,7 +14,21 @@ eval "$(conda shell.bash hook)"
 conda activate ./envs_pyqreg
 cd code
 
-python -u ./calc_mon-p095.py --start=1979 --end=2020 --dataset="gpcc" --q=0.95
+for start_year in $(seq 1930 1990); do
+    for end_year in $(seq $((start_year + 30)) 2020); do
+    
+        python -u ./calc_mon-p095.py --start=$start_year --end=$end_year --dataset="gpcc" --q=0.95
+        python -u ./calc_mon-p095.py --start=$start_year --end=$end_year --dataset="cmip" --q=0.95
+        python -u ./calc_mon-p095.py --start=$start_year --end=$end_year --dataset="mesaclip" --q=0.95
+        python -u ./calc_mon-p095.py --start=$start_year --end=$end_year --dataset="spear" --q=0.95
+
+        if [ "$start_year" -ge 1979]; then
+           python -u ./calc_mon-p095.py --start=$start_year --end=end_year --dataset="gpcp"
+           python -u ./calc_mon-p095.py --start=$start_year --end=end_year --dataset="mswep"
+        fi
+        
+    done
+done
 
 
 
--- sbatch/calc_rx1day.sh
+++ sbatch/calc_rx1day.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=/davenport-scratch/fvdav22/job_output/calc_rx1day.out
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=f.davenport@colostate.edu
-#SBATCH --ntasks=16
+#SBATCH --ntasks=32
 #SBATCH --time=40:00:00
 #SBATCH -p dav+coe
 
@@ -14,31 +14,33 @@ eval "$(conda shell.bash hook)"
 conda activate ./envs
 cd code
 
-for start_year in {1950..1975..5}; do
+#for start_year in {1950..1975..5}; do
 
-    # calculate trends through 2016 for all models and REGEN
-    #python -u ./calc_rx1day.py --start=$start_year --end=2016 --dataset="regen"
-    python -u ./calc_rx1day.py --start=$start_year --end=2016 --dataset="cmip"
-    #python -u ./calc_rx1day.py --start=$start_year --end=2016 --dataset="highres_cmip"
-    #python -u ./calc_rx1day.py --start=$start_year --end=2016 --dataset="mesaclip"
-    #python -u ./calc_rx1day.py --start=$start_year --end=2016 --dataset="spear"
+for start_year in $(seq 1950 1990); do
+    for end_year in $(seq $((start_year + 30)) 2020); do
     
-    if [ "$start_year" -lt 1975 ]; then
-        # calculate 45-year trends
-        python -u ./calc_rx1day.py --start=$start_year --end=$((start_year + 41)) --dataset="regen"
-        python -u ./calc_rx1day.py --start=$start_year --end=$((start_year + 41)) --dataset="cmip"
-        #python -u ./calc_rx1day.py --start=$start_year --end=$((start_year + 41)) --dataset="highres_cmip"
-        #python -u ./calc_rx1day.py --start=$start_year --end=$((start_year + 41)) --dataset="mesaclip"
-        #python -u ./calc_rx1day.py --start=$start_year --end=$((start_year + 41)) --dataset="spear"
+        # calculate trends through 2016 for all models and REGEN
+        python -u ./calc_rx1day.py --start=$start_year --end=end_year --dataset="cmip"
+        python -u ./calc_rx1day.py --start=$start_year --end=end_year --dataset="mesaclip"
+        python -u ./calc_rx1day.py --start=$start_year --end=end_year --dataset="spear"
+    
+    if [ "$end_year" -le 2016]; then
+       python -u ./calc_rx1day.py --start=$start_year --end=end_year --dataset="regen"
+    fi
+    
+    if [ "$start_year" -ge 1979]; then
+       python -u ./calc_rx1day.py --start=$start_year --end=end_year --dataset="cpc"
+       python -u ./calc_rx1day.py --start=$start_year --end=end_year --dataset="mswep"
     fi
 
+    done
 done
 
 #python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="regen"
 #python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="cpc"
 #python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="cpc_shift"
 #python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="mswep"
-python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="cmip"
+#python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="cmip"
 #python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="highres_cmip"
 #python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="mesaclip"
 #python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="spear"
@@ -46,7 +48,7 @@ python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="cmip"
 #python -u ./calc_rx1day.py --start=1979 --end=2020 --dataset="cpc"
 #python -u ./calc_rx1day.py --start=1979 --end=2020 --dataset="cpc_shift"
 #python -u ./calc_rx1day.py --start=1979 --end=2020 --dataset="mswep"
-python -u ./calc_rx1day.py --start=1979 --end=2020 --dataset="cmip"
+#python -u ./calc_rx1day.py --start=1979 --end=2020 --dataset="cmip"
 #python -u ./calc_rx1day.py --start=1979 --end=2020 --dataset="highres_cmip"
 #python -u ./calc_rx1day.py --start=1979 --end=2020 --dataset="mesaclip"
 #python -u ./calc_rx1day.py --start=1979 --end=2020 --dataset="spear"
@@ -54,7 +56,7 @@ python -u ./calc_rx1day.py --start=1979 --end=2020 --dataset="cmip"
 #python -u ./calc_rx1day.py --start=1979 --end=2024 --dataset="cpc"
 #python -u ./calc_rx1day.py --start=1979 --end=2024 --dataset="cpc_shift"
 #python -u ./calc_rx1day.py --start=1979 --end=2024 --dataset="mswep"
-python -u ./calc_rx1day.py --start=1979 --end=2024 --dataset="cmip"
+#python -u ./calc_rx1day.py --start=1979 --end=2024 --dataset="cmip"
 #python -u ./calc_rx1day.py --start=1979 --end=2024 --dataset="highres_cmip"
 #python -u ./calc_rx1day.py --start=1979 --end=2024 --dataset="mesaclip"
 #python -u ./calc_rx1day.py --start=1979 --end=2024 --dataset="spear"
