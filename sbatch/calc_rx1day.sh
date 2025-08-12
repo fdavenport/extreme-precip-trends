b6d57bf86ebe8818d67a471b39ee814a74f85928
Frances Davenport <f.davenport@colostate.edu>
2025-08-12 11:56:07 -0600

    change to array job

--- sbatch/calc_rx1day.sh
+++ sbatch/calc_rx1day.sh
@@ -4,9 +4,11 @@
 #SBATCH --output=/davenport-scratch/fvdav22/job_output/calc_rx1day.out
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=f.davenport@colostate.edu
-#SBATCH --ntasks=32
-#SBATCH --time=40:00:00
-#SBATCH -p dav_all,coe_all
+#SBATCH --ntasks=8
+#SBATCH --time=5:00:00
+#SBATCH --array=0-40%8
+#SBATCH -p dav_all
+#SBATCH --nodelist=davenport-cpu1
 
 
 cd /davenport-scratch/fvdav22/projects/extreme-precip-trends
@@ -14,15 +16,14 @@ eval "$(conda shell.bash hook)"
 conda activate ./envs
 cd code
 
-#for start_year in {1950..1975..5}; do
+start_year=$((SLURM_ARRAY_TASK_ID+1950))
 
-for start_year in $(seq 1950 1990); do
-    for end_year in $(seq $((start_year + 30)) 2020); do
+for end_year in $(seq $((start_year + 30)) 2020); do
     
-        # calculate trends through 2016 for all models and REGEN
-        python -u ./calc_rx1day.py --start=$start_year --end=$end_year --dataset="cmip"
-        python -u ./calc_rx1day.py --start=$start_year --end=$end_year --dataset="mesaclip"
-        python -u ./calc_rx1day.py --start=$start_year --end=$end_year --dataset="spear"
+    # calculate trends through 2016 for all models and REGEN
+    python -u ./calc_rx1day.py --start=$start_year --end=$end_year --dataset="cmip"
+    python -u ./calc_rx1day.py --start=$start_year --end=$end_year --dataset="mesaclip"
+    python -u ./calc_rx1day.py --start=$start_year --end=$end_year --dataset="spear"
     
     if [ "$end_year" -le 2016 ]; then
        python -u ./calc_rx1day.py --start=$start_year --end=$end_year --dataset="regen"
@@ -33,7 +34,6 @@ for start_year in $(seq 1950 1990); do
        python -u ./calc_rx1day.py --start=$start_year --end=$end_year --dataset="mswep"
     fi
 
-    done
 done
 
 #python -u ./calc_rx1day.py --start=1979 --end=2016 --dataset="regen"
