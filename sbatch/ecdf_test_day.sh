7e1161e0e9d415d33966a42083806175150a173c
Frances Davenport <f.davenport@colostate.edu>
2025-08-15 13:52:57 -0600

    sbatch files for ecdf testing

---
+++ sbatch/ecdf_test_day.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=ecdf_test_day
+#SBATCH --error=/davenport-scratch/fvdav22/job_output/ecdf_test_day_%a.err
+#SBATCH --output=/davenport-scratch/fvdav22/job_output/ecdf_test_day_%a.out
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=f.davenport@colostate.edu
+#SBATCH --ntasks=4
+#SBATCH --time=3:00:00
+#SBATCH --array=0-40
+#SBATCH --partition=all
+
+cd /davenport-scratch/fvdav22/projects/extreme-precip-trends
+eval "$(conda shell.bash hook)"
+conda activate ./envs
+cd code
+
+start_year=$((SLURM_ARRAY_TASK_ID+1950))
+
+for end_year in $(seq $((start_year + 30)) 2016); do
+    
+    python -u ./calc_ecdf.py --start=$start_year --end=$end_year --obs="regen" --model="cmip-sub" --var="rx1day" --test
+    python -u ./calc_ecdf.py --start=$start_year --end=$end_year --obs="regen" --model="spear" --var="rx1day" --test
+    python -u ./calc_ecdf.py --start=$start_year --end=$end_year --obs="regen" --model="mesaclip" --var="rx1day" --test
+    
+
+done
+
+
+
---
+++ sbatch/ecdf_test_mon-p095.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=ecdf_test_mon
+#SBATCH --error=/davenport-scratch/fvdav22/job_output/ecdf_test_mon_%a.err
+#SBATCH --output=/davenport-scratch/fvdav22/job_output/ecdf_test_mon_%a.out
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=f.davenport@colostate.edu
+#SBATCH --ntasks=4
+#SBATCH --time=3:00:00
+#SBATCH --array=0-60
+#SBATCH --partition=all
+
+
+cd /davenport-scratch/fvdav22/projects/extreme-precip-trends
+eval "$(conda shell.bash hook)"
+conda activate ./envs
+cd code
+
+start_year=$((SLURM_ARRAY_TASK_ID+1930))
+
+for end_year in $(seq $((start_year + 30)) 2020); do
+    
+    python -u ./calc_ecdf.py --start=$start_year --end=$end_year --obs="gpcc" --model="cmip-sub" --var="mon-p095" --test
+    python -u ./calc_ecdf.py --start=$start_year --end=$end_year --obs="gpcc" --model="spear" --var="mon-p095" --test
+    python -u ./calc_ecdf.py --start=$start_year --end=$end_year --obs="gpcc" --model="mesaclip" --var="mon-p095" --test
+    
+done
+
+
+
