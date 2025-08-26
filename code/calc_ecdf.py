fb278945d5833c46c4866f092020fee5b24eab8b
Frances Davenport <f.davenport@colostate.edu>
2025-08-26 15:57:54 -0600

    add obs quality mask option

--- code/calc_ecdf.py
+++ code/calc_ecdf.py
@@ -9,6 +9,7 @@ import _utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--overwrite', action='store_true') #don't overwrite unless overwrite specified
+parser.add_argument('--obs_mask', action='store_true') #use obs mask if specified
 parser.add_argument('--test', action='store_true') #specify whether to do test or regular calculation
 parser.add_argument('--start', type=int)
 parser.add_argument('--end', type=int)
@@ -34,6 +35,10 @@ common_mask = xr.open_dataset("../processed_data/common_land_mask.nc")
 def mask(ds):
     return(xr.where(common_mask.__xarray_dataarray_variable__ == 1, ds, np.nan))
 
+regen_mask = xr.open_dataset("../processed_data/regen_LongTerm_quality_mask.nc")
+def gauge_mask(ds):
+    return(xr.where(regen_mask.p >= 2, ds, np.nan))
+
 obs_trend = _utils.read_trends("../processed_data/", args.obs, args.var, start, end)
 model_trend = _utils.read_trends("../processed_data/", model, args.var, start, end)
 if (args.model == "cmip-sub") & (args.var == "rx1day"):
@@ -45,7 +50,11 @@ if not args.test:
     ecdf_result = _utils.ecdf_xr(model_trend, obs_trend)
     ecdf_result.to_netcdf("../processed_data/ecdf/"+args.obs+"_"+args.model+"_"+args.var+"_"+str(start)+"-"+str(end)+"_ecdf.nc")
 else: 
-    ecdf_test = _utils.test_ecdf(mask(model_trend), mask(obs_trend))
-    ecdf_test.to_csv("../processed_data/ecdf/"+args.obs+"_"+args.model+"_"+args.var+"_"+str(start)+"-"+str(end)+"_ecdf_test.csv")
+    if args.obs_mask: 
+        ecdf_test = _utils.test_ecdf(gauge_mask(model_trend), gauge_mask(obs_trend))
+        ecdf_test.to_csv("../processed_data/ecdf/"+args.obs+"_"+args.model+"_"+args.var+"_"+str(start)+"-"+str(end)+"_ecdf_test_gauge_mask.csv")
+    else: 
+        ecdf_test = _utils.test_ecdf(mask(model_trend), mask(obs_trend))
+        ecdf_test.to_csv("../processed_data/ecdf/"+args.obs+"_"+args.model+"_"+args.var+"_"+str(start)+"-"+str(end)+"_ecdf_test.csv")
     
     
